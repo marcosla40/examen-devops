2494f1d52c8b2d9e9aa4c4db566f5e4297f8f62b
marcosla40 <marcosla40@educastur.es>
2025-11-21 17:47:01 +0100

    Primer commit

---
+++ src/test/java/com/devops/examen/ExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
